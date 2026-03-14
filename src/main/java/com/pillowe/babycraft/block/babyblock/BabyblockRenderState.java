e0de65ab159d17954021e61ef1dd2d3288cadc52
Pillowe <pillowe@163.com>
2026-03-14 20:02:25 +0800

    refactored babyblock rendering using submitMovingBlock()

--- src/main/java/com/pillowe/babycraft/block/babyblock/BabyblockRenderState.java
+++ src/main/java/com/pillowe/babycraft/block/babyblock/BabyblockRenderState.java
@@ -1,5 +1,6 @@
 package com.pillowe.babycraft.block.babyblock;
 
+import net.minecraft.client.renderer.block.MovingBlockRenderState;
 import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
@@ -10,6 +11,7 @@ public class BabyblockRenderState extends BlockEntityRenderState {
     public BlockPos pos;
     public int packedLight;
     public float growState;
+    public final MovingBlockRenderState movingState = new MovingBlockRenderState();
 
     public BabyblockRenderState() {
         // Initialize with a default state
--- src/main/java/com/pillowe/babycraft/block/babyblock/BabyblockRenderer.java
+++ src/main/java/com/pillowe/babycraft/block/babyblock/BabyblockRenderer.java
@@ -1,12 +1,13 @@
 package com.pillowe.babycraft.block.babyblock;
 
-import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
-import net.minecraft.client.renderer.state.CameraRenderState;
-import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.renderer.state.level.CameraRenderState;// It was moved into level
+
+import net.minecraft.world.level.CardinalLighting;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 
 import org.jetbrains.annotations.Nullable;
@@ -24,6 +25,7 @@ public class BabyblockRenderer implements BlockEntityRenderer<BabyblockEntity, B
     public BabyblockRenderState createRenderState() {
         // Create the render state
         return new BabyblockRenderState();
+
     }
 
     @Override
@@ -34,21 +36,23 @@ public class BabyblockRenderer implements BlockEntityRenderer<BabyblockEntity, B
             @Nullable ModelFeatureRenderer.CrumblingOverlay overlay) {
 
         // Extract the adult state from block entity
-        BlockEntityRenderer.super.extractRenderState(blockEntity,
-                renderState,
-                partialTicks,
-                cameraPos,
+        BlockEntityRenderer.super.extractRenderState(blockEntity, renderState, partialTicks, cameraPos,
                 overlay); // This is mandatory, or die alive no render
         renderState.adultState = blockEntity.getAdultState();
         renderState.growState = blockEntity.getGrowState();
         renderState.pos = blockEntity.getBlockPos();
+        Level level = blockEntity.getLevel();
 
-        // Make it light as common blocks
-        if (blockEntity.getLevel() != null) {
-            renderState.packedLight = LevelRenderer.getLightCoords(blockEntity.getLevel(), renderState.pos);
-        } else {
-            renderState.packedLight = 15728880;
-        }
+        // Set the moving block state
+        renderState.movingState.blockState = renderState.adultState;
+        renderState.movingState.blockPos = blockEntity.getBlockPos();
+        renderState.movingState.biome = level.getBiome(blockEntity.getBlockPos());
+        renderState.movingState.lightEngine = level.getLightEngine();
+        renderState.movingState.cardinalLighting = CardinalLighting.DEFAULT;
+        renderState.movingState.randomSeedPos = renderState.pos;
+
+        ;
+
     }
 
     @Override
@@ -56,24 +60,20 @@ public class BabyblockRenderer implements BlockEntityRenderer<BabyblockEntity, B
             PoseStack poseStack,
             SubmitNodeCollector collector,
             CameraRenderState cameraState) {
-
         // Submit the adult block
         if (state.adultState == null || state.adultState.isAir()) {
             return;
         }
+
         // Cal the grow state
         float scaleMultiplier = 0.3f + 0.15f * state.growState;
         // Matrix transform to get a smaller one
         poseStack.pushPose();
-        poseStack.translate(0.5, 0, 0.5);
+        poseStack.translate(0.5, 0.0001, 0.5);
         poseStack.scale(scaleMultiplier, scaleMultiplier, scaleMultiplier);
         poseStack.translate(-0.5, 0, -0.5);
 
-        collector.submitBlock(poseStack,
-                state.adultState,
-                state.packedLight,
-                OverlayTexture.NO_OVERLAY,
-                highlighted ? 255 : 0);
+        collector.submitMovingBlock(poseStack, state.movingState);
 
         poseStack.popPose();
     }
