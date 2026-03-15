b05bc151af3ef29d4ca2d9d0a0bb9c31f5b31c1e
Pillowe <pillowe@163.com>
2026-03-15 22:01:48 +0800

    You can rejuvenate ageing mobs using dandelioness now

--- src/main/java/com/pillowe/babycraft/item/golden_dandelioness/GoldenDandelionessItem.java
+++ src/main/java/com/pillowe/babycraft/item/golden_dandelioness/GoldenDandelionessItem.java
@@ -4,11 +4,16 @@ import com.pillowe.babycraft.block.ModBlocks;
 import com.pillowe.babycraft.block.babyblock.BabyblockEntity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 
 public class GoldenDandelionessItem extends Item {
     public GoldenDandelionessItem(Properties properties) {
@@ -30,6 +35,18 @@ public class GoldenDandelionessItem extends Item {
         }
 
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public InteractionResult interactLivingEntity(ItemStack itemStack, Player player, LivingEntity target,
+            InteractionHand type) {
+        System.out.println("interactLivingEntity");
+        if (target instanceof AgeableMob ageableMob) {
+            ageableMob.setBaby(true);
+            itemStack.shrink(1);
+            return InteractionResult.SUCCESS;
+        }
+        return super.interactLivingEntity(itemStack, null, target, type);
     }
 
 }
