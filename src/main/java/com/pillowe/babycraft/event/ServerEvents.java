afb575a64dfb6cda00949e809e00b1d1f8dea1e7
Pillowe <pillowe@163.com>
2026-03-28 22:55:00 +0800

    now attacks from mobs with mini effect apply a rej effect

--- src/main/java/com/pillowe/babycraft/event/ServerEvents.java
+++ src/main/java/com/pillowe/babycraft/event/ServerEvents.java
@@ -7,30 +7,33 @@ import com.pillowe.babycraft.effect.ModEffects;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
 
-@EventBusSubscriber(modid = BabycraftMod.MOD_ID, value = Dist.DEDICATED_SERVER)
+@EventBusSubscriber(modid = BabycraftMod.MOD_ID)
 public class ServerEvents {
     @SubscribeEvent
-    public void checkMinimize(LivingIncomingDamageEvent event) {
+    public static void checkMinimize(LivingIncomingDamageEvent event) {
+        System.out.println("checkMinimize");
         Level level = event.getEntity().level();
+        if (level.isClientSide()) {
+            return;
+        }
         if (event.getSource().getEntity() instanceof LivingEntity attacker) {
             if (!attacker.hasEffect(ModEffects.MINIMIZE_EFFECT.getDelegate())) {
                 return;
             }
             int amplifier = attacker.getEffect(ModEffects.MINIMIZE_EFFECT.getDelegate()).getAmplifier();
-
+            System.out.println("amplifier: " + amplifier);
             if (level.getRandom().nextDouble() < Config.MINIMIZE_BASE_CHANCE.get()
                     + Config.MINIMIZE_LEVEL_CHANCE.get()
-                            * (amplifier - 1)) {
-
+                            * amplifier) {
+                System.out.println("add rejuvenation");
                 event.getEntity().addEffect(
                         new MobEffectInstance(
                                 ModEffects.REJUVENATION,
-                                amplifier * Config.MINIMIZE_LEVEL_DURATION.get()));
+                                (amplifier + 1) * Config.MINIMIZE_LEVEL_DURATION.get()));
 
             }
         }
